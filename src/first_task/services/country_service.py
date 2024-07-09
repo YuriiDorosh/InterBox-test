92b69614df98135c065e172c929354f240c87288
Yurii <yuriydorosh2005@gmail.com>
2024-07-09 12:24:33 +0300

    UPDATE: services(new services using repository)

---
+++ src/first_task/services/country_service.py
@@ -0,0 +1,10 @@
+from typing import List
+from models.country_info import CountryInfo
+from repositories.country_repository import CountryRepository
+
+class CountryService:
+    def __init__(self, repository: CountryRepository):
+        self.repository = repository
+
+    def get_country_data(self) -> List[CountryInfo]:
+        return self.repository.fetch_data()
