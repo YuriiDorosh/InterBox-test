367cb3f94e0598d1844bff2e66ce3c340ca0a045
Yurii <yuriydorosh2005@gmail.com>
2024-07-09 15:02:01 +0300

    UPDATE: first_task(settings.py: redis comment)

--- src/first_task/settings.py
+++ src/first_task/settings.py
@@ -2,7 +2,7 @@ class Settings:
     API_BASE_URL = "https://restcountries.com/v3.1/all"
     CACHE_DURATION = 3600  # 1 година
     USE_CACHE = True  # По дефолту залишаємо кешування увімкненим
-    REDIS_HOST = "redis"
+    REDIS_HOST = "redis" # Поміняти на localhost, якщо ви запускаєте Redis не в докері
     REDIS_PORT = 6379
     REDIS_DB = 0
     REDIS_CACHE_KEY = "country_data"
