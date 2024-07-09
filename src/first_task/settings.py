aa7ad920998081d3bd5f84cac8cb36931a861608
Yurii <yuriydorosh2005@gmail.com>
2024-07-09 12:24:44 +0300

    UPDATE: settings.py(class)

--- src/first_task/settings.py
+++ src/first_task/settings.py
@@ -1,7 +1,8 @@
-API_BASE_URL = "https://restcountries.com/v3.1/all"
-CACHE_DURATION = 3600  # 1 година
-USE_CACHE = True  # По дефолту залишаємо кешування увімкненим
-REDIS_HOST = "redis"
-REDIS_PORT = 6379
-REDIS_DB = 0
-REDIS_CACHE_KEY = "country_data"
+class Settings:
+    API_BASE_URL = "https://restcountries.com/v3.1/all"
+    CACHE_DURATION = 3600  # 1 година
+    USE_CACHE = True  # По дефолту залишаємо кешування увімкненим
+    REDIS_HOST = "redis"
+    REDIS_PORT = 6379
+    REDIS_DB = 0
+    REDIS_CACHE_KEY = "country_data"
