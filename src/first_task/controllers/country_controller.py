a1426c03122ebaf6c9d57a3a36cc00ac0e223c1e
Yurii <yuriydorosh2005@gmail.com>
2024-07-09 12:24:57 +0300

    ADD: controllers(print data)

---
+++ src/first_task/controllers/country_controller.py
@@ -0,0 +1,16 @@
+from services.country_service import CountryService
+from prettytable import PrettyTable
+
+class CountryController:
+    def __init__(self, service: CountryService):
+        self.service = service
+
+    def display_data(self) -> None:
+        countries = self.service.get_country_data()
+        table = PrettyTable()
+        table.field_names = ["Country Name", "Capital", "Flag URL"]
+
+        for country in countries:
+            table.add_row([country.name, country.capital, country.flag_url])
+
+        print(table)
